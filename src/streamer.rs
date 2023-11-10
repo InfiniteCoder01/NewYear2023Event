c825a63dae916911be8a588d8cfdabdf6609b945
InfiniteCoder <nayka.0.lobach.01@gmail.com>
2023-11-10 22:37:50 +0300

    No logs

--- src/streamer.rs
+++ src/streamer.rs
@@ -62,43 +62,43 @@ pub fn stream(width: usize, height: usize, fps: usize, rtmp_uri: &str) {
         }
     });
 
-    for message in pipeline.bus().unwrap().receiver().iter() {
-        match message.parse() {
-            gst::Message::StateChangedParsed {
-                ref old, ref new, ..
-            } => {
-                println!(
-                    "element `{}` changed from {:?} to {:?}",
-                    message.src_name(),
-                    old,
-                    new
-                );
-            }
-            gst::Message::ErrorParsed {
-                ref error,
-                ref debug,
-                ..
-            } => {
-                println!(
-                    "error msg from element `{}`: {}, {}. Quitting",
-                    message.src_name(),
-                    error.message(),
-                    debug
-                );
-                break;
-            }
-            gst::Message::Eos(_) => {
-                println!("eos received quiting");
-                break;
-            }
-            _ => {
-                println!(
-                    "msg of type `{}` from element `{}`",
-                    message.type_name(),
-                    message.src_name()
-                );
-            }
-        }
+    for _message in pipeline.bus().unwrap().receiver().iter() {
+        // match message.parse() {
+        //     gst::Message::StateChangedParsed {
+        //         ref old, ref new, ..
+        //     } => {
+        //         println!(
+        //             "element `{}` changed from {:?} to {:?}",
+        //             message.src_name(),
+        //             old,
+        //             new
+        //         );
+        //     }
+        //     gst::Message::ErrorParsed {
+        //         ref error,
+        //         ref debug,
+        //         ..
+        //     } => {
+        //         println!(
+        //             "error msg from element `{}`: {}, {}. Quitting",
+        //             message.src_name(),
+        //             error.message(),
+        //             debug
+        //         );
+        //         break;
+        //     }
+        //     gst::Message::Eos(_) => {
+        //         println!("eos received quiting");
+        //         break;
+        //     }
+        //     _ => {
+        //         println!(
+        //             "msg of type `{}` from element `{}`",
+        //             message.type_name(),
+        //             message.src_name()
+        //         );
+        //     }
+        // }
     }
     mainloop.quit();
 }

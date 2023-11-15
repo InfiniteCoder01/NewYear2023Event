3ae193f53586f697b553d7fa7a79a91a4413efbf
InfiniteCoder <nayka.0.lobach.01@gmail.com>
2023-11-15 17:36:30 +0300

    Cairo - Simpler text

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
-pub mod renderer;
 pub mod streamer;
 
-use crate::renderer::*;
 use hhmmss::Hhmmss;
 use serde::Deserialize;
 
@@ -14,12 +12,6 @@ fn main() {
     let private: Private =
         toml::from_str(&std::fs::read_to_string("private.toml").unwrap()).unwrap();
 
-    let font = fontdue::Font::from_bytes(
-        include_bytes!("../Assets/Roobert-Regular.ttf") as &[u8],
-        fontdue::FontSettings::default(),
-    )
-    .unwrap();
-    let fonts = [font];
     let stream_start = std::time::Instant::now();
     let mut frame_index = 0;
     let mut last_render_time = 0;
@@ -31,31 +23,33 @@ fn main() {
         // 128000,
         24000,
         &format!("rtmp://a.rtmp.youtube.com/live2/{}", private.key),
-        move |frame| {
-            let render_start = std::time::Instant::now(); // ! Profiling
+        move |context, width, height| {
+            let render_start = std::time::Instant::now();
             let uptime = stream_start.elapsed();
 
-            frame.clear(Color::BLACK);
-            frame.draw_text(
-                10,
-                10,
-                &format!(
-                    "Frame {frame_index}\nUptime: {}\nRendered in {}ms\nMax possible framerate: {:.2}",
-                    uptime.hhmmssxxx(),
-                    last_render_time / 1000,
-                    1_000_000.0 / last_render_time as f32,
-                ),
-                Color::WHITE,
-                30.0,
-                &fonts,
+            context.set_source_rgb(1.0, 1.0, 1.0);
+            context.select_font_face(
+                "Purisa",
+                cairo::FontSlant::Normal,
+                cairo::FontWeight::Normal,
             );
+            context.set_font_size(20.0);
+            context.move_to(20.0, 30.0);
+            context
+                .show_text(&format!(
+                "Frame {frame_index}\nUptime: {}\nRendered in {}ms\nMax possible framerate: {:.2}",
+                uptime.hhmmssxxx(),
+                last_render_time / 1000,
+                1_000_000.0 / last_render_time as f32,
+            ))
+                .unwrap();
 
-            frame.fill_rect(
-                frame.width as i32 / 2 + (uptime.as_secs_f32().sin() * 100.0) as i32 - 50,
-                frame.height as i32 / 2 - 50,
-                100,
-                100,
-                Color::RED,
+            context.set_source_rgb(1.0, 0.0, 0.0);
+            context.rectangle(
+                width / 2.0 + uptime.as_secs_f64().sin() * 100.0 - 50.0,
+                height / 2.0 - 50.0,
+                100.0,
+                100.0,
             );
 
             last_render_time = render_start.elapsed().as_micros();
--- src/streamer.rs
+++ src/streamer.rs
@@ -2,14 +2,13 @@ extern crate gstreamer as gst;
 extern crate gstreamer_app as gst_app;
 extern crate gstreamer_video as gst_video;
 
-use crate::renderer::*;
 use gst::{prelude::*, Caps, ElementFactory};
 
 pub fn stream(
     size: (usize, usize),
     audio_bitrate: usize,
     rtmp_uri: &str,
-    mut draw_frame: impl FnMut(&mut Frame) + Send + Sync + 'static,
+    mut draw_frame: impl FnMut(cairo::Context, f64, f64) + Send + Sync + 'static,
 ) {
     // let pipeline_str = format!(
     //     concat!(
@@ -115,84 +114,11 @@ pub fn stream(
 
     // * Draw callback
     video_overlay.connect("draw", false, move |args| {
-        // let drawer = &drawer_clone;
-        // let drawer = drawer.lock().unwrap();
-
-        let cr = args[1].get::<cairo::Context>().unwrap();
-        // let timestamp = args[2].get::<gst::ClockTime>().unwrap();
-
-        cr.set_source_rgb(1.0, 1.0, 1.0);
-        cr.select_font_face(
-            "Purisa",
-            cairo::FontSlant::Normal,
-            cairo::FontWeight::Normal,
+        draw_frame(
+            args[1].get::<cairo::Context>().unwrap(),
+            width as _,
+            height as _,
         );
-        cr.set_font_size(13.0);
-        cr.move_to(20.0, 30.0);
-        cr.show_text("Hello, world!").unwrap();
-
-        // let info = drawer.info.as_ref().unwrap();
-        // let layout = &drawer.layout;
-
-        // let angle = 2.0 * PI * (timestamp % (10 * gst::ClockTime::SECOND)).nseconds() as f64
-        //     / (10.0 * gst::ClockTime::SECOND.nseconds() as f64);
-
-        // // The image we draw (the text) will be static, but we will change the
-        // // transformation on the drawing context, which rotates and shifts everything
-        // // that we draw afterwards. Like this, we have no complicated calculations
-        // // in the actual drawing below.
-        // // Calling multiple transformation methods after each other will apply the
-        // // new transformation on top. If you repeat the cr.rotate(angle) line below
-        // // this a second time, everything in the canvas will rotate twice as fast.
-        // cr.translate(
-        //     f64::from(info.width()) / 2.0,
-        //     f64::from(info.height()) / 2.0,
-        // );
-        // cr.rotate(angle);
-
-        // // This loop will render 10 times the string "GStreamer" in a circle
-        // for i in 0..10 {
-        //     // Cairo, like most rendering frameworks, is using a stack for transformations
-        //     // with this, we push our current transformation onto this stack - allowing us
-        //     // to make temporary changes / render something / and then returning to the
-        //     // previous transformations.
-        //     cr.save().expect("Failed to save state");
-
-        //     let angle = (360. * f64::from(i)) / 10.0;
-        //     let red = (1.0 + f64::cos((angle - 60.0) * PI / 180.0)) / 2.0;
-        //     cr.set_source_rgb(red, 0.0, 1.0 - red);
-        //     cr.rotate(angle * PI / 180.0);
-
-        //     // Update the text layout. This function is only updating pango's internal state.
-        //     // So e.g. that after a 90 degree rotation it knows that what was previously going
-        //     // to end up as a 200x100 rectangle would now be 100x200.
-        //     pangocairo::functions::update_layout(&cr, layout);
-        //     let (width, _height) = layout.size();
-        //     // Using width and height of the text, we can properly position it within
-        //     // our canvas.
-        //     cr.move_to(
-        //         -(f64::from(width) / f64::from(pango::SCALE)) / 2.0,
-        //         -(f64::from(info.height())) / 2.0,
-        //     );
-        //     // After telling the layout object where to draw itself, we actually tell
-        //     // it to draw itself into our cairo context.
-        //     pangocairo::functions::show_layout(&cr, layout);
-
-        //     // Here we go one step up in our stack of transformations, removing any
-        //     // changes we did to them since the last call to cr.save();
-        //     cr.restore().expect("Failed to restore state");
-        // }
-
-        None
-    });
-
-    video_overlay.connect("caps-changed", false, move |args| {
-        let _overlay = args[0].get::<gst::Element>().unwrap();
-        let caps = args[1].get::<gst::Caps>().unwrap();
-
-        // let mut drawer = drawer.lock().unwrap();
-        // drawer.info = Some(gst_video::VideoInfo::from_caps(&caps).unwrap());
-
         None
     });
 

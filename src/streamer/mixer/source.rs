663cfc89980d0f42932bb599e7981d2c7f8ea640
InfiniteCoder <nayka.0.lobach.01@gmail.com>
2023-11-19 19:12:07 +0300

    Really hard audio

--- src/streamer/mixer/source.rs
+++ src/streamer/mixer/source.rs
@@ -9,7 +9,7 @@ mod imp {
 
     #[derive(Default)]
     pub struct MixerSource {
-        // buffer: std::sync::Mutex<Vec<u8>>,
+        buffer: std::sync::Mutex<Vec<u8>>,
     }
 
     #[glib::object_subclass]
@@ -66,68 +66,69 @@ mod imp {
             &self,
             _buffer: Option<&mut gst::BufferRef>,
         ) -> Result<gst_base::subclass::base_src::CreateSuccess, gst::FlowError> {
-            let mut audio_mixer = unsafe { MIXER.as_ref() }.unwrap().lock().unwrap();
-            let mut samples = vec![0_i16; 2];
-            if let Some(voice) = &mut audio_mixer.voice {
-                match voice.next_frame() {
-                    Ok(minimp3::Frame {
-                        data,
-                        sample_rate: _,
-                        channels: _,
-                        ..
-                    }) => samples = data,
-                    Err(minimp3::Error::Eof) => audio_mixer.voice = None,
-                    Err(e) => {
-                        eprintln!("{:?}", e);
-                        audio_mixer.voice = None;
-                    }
-                }
-            }
-            let buffer = gst::Buffer::from_slice(unsafe {
-                std::slice::from_raw_parts(samples.as_ptr() as *const u8, samples.len() * 2)
-            });
-            Ok(gst_base::subclass::base_src::CreateSuccess::NewBuffer(
-                buffer,
-            ))
+            // let mut audio_mixer = unsafe { MIXER.as_ref() }.unwrap().lock().unwrap();
+            // let mut samples = vec![0_i16; 2];
+            // if let Some(voice) = &mut audio_mixer.voice {
+            //     match voice.next_frame() {
+            //         Ok(minimp3::Frame {
+            //             data,
+            //             sample_rate: _,
+            //             channels: _,
+            //             ..
+            //         }) => samples = data,
+            //         Err(minimp3::Error::Eof) => audio_mixer.voice = None,
+            //         Err(e) => {
+            //             eprintln!("{:?}", e);
+            //             audio_mixer.voice = None;
+            //         }
+            //     }
+            // }
+            // let buffer = gst::Buffer::from_slice(unsafe {
+            //     std::slice::from_raw_parts(samples.as_ptr() as *const u8, samples.len() * 2)
+            // });
+            // Ok(gst_base::subclass::base_src::CreateSuccess::NewBuffer(
+            //     buffer,
+            // ))
+            Ok(gst_base::subclass::base_src::CreateSuccess::FilledBuffer)
         }
 
-        // fn fill(&self, buffer: &mut gst::BufferRef) -> Result<gst::FlowSuccess, gst::FlowError> {
-        //     let mut audio_mixer = unsafe { MIXER.as_ref() }.unwrap().lock().unwrap();
-        //     let mut inner_buffer = self.buffer.lock().unwrap();
-        //     while inner_buffer.len() < buffer.size() {
-        //         if let Some(voice) = &mut audio_mixer.voice {
-        //             match voice.next_frame() {
-        //                 Ok(minimp3::Frame {
-        //                     data,
-        //                     sample_rate: _,
-        //                     channels: _,
-        //                     ..
-        //                 }) => {
-        //                     inner_buffer
-        //                         .extend(data.iter().flat_map(|sample| sample.to_le_bytes()));
-        //                     continue;
-        //                 }
-        //                 Err(minimp3::Error::Eof) => {
-        //                     audio_mixer.voice = None;
-        //                 }
-        //                 Err(e) => {
-        //                     eprintln!("{:?}", e);
-        //                     audio_mixer.voice = None;
-        //                 }
-        //             }
-        //         }
-        //         let need = buffer.size() - inner_buffer.len();
-        //         inner_buffer.extend(std::iter::repeat(0).take(need));
-        //     }
-        //     let need = buffer.size();
-        //     buffer
-        //         .map_writable()
-        //         .unwrap()
-        //         .as_mut_slice()
-        //         .copy_from_slice(&inner_buffer[..need]);
-        //     inner_buffer.drain(..need);
-        //     Ok(gst::FlowSuccess::Ok)
-        // }
+        fn fill(&self, buffer: &mut gst::BufferRef) -> Result<gst::FlowSuccess, gst::FlowError> {
+            let mut audio_mixer = unsafe { MIXER.as_ref() }.unwrap().lock().unwrap();
+            let mut inner_buffer = self.buffer.lock().unwrap();
+            while inner_buffer.len() < buffer.size() {
+                if let Some(voice) = &mut audio_mixer.voice {
+                    match voice.next_frame() {
+                        Ok(minimp3::Frame {
+                            data,
+                            sample_rate: _,
+                            channels: _,
+                            ..
+                        }) => {
+                            inner_buffer
+                                .extend(data.iter().flat_map(|sample| sample.to_le_bytes()));
+                            continue;
+                        }
+                        Err(minimp3::Error::Eof) => {
+                            audio_mixer.voice = None;
+                        }
+                        Err(e) => {
+                            eprintln!("{:?}", e);
+                            audio_mixer.voice = None;
+                        }
+                    }
+                }
+                let need = buffer.size() - inner_buffer.len();
+                inner_buffer.extend(std::iter::repeat(0).take(need));
+            }
+            let need = buffer.size();
+            buffer
+                .map_writable()
+                .unwrap()
+                .as_mut_slice()
+                .copy_from_slice(&inner_buffer[..need]);
+            inner_buffer.drain(..need);
+            Ok(gst::FlowSuccess::Ok)
+        }
     }
 }
 
